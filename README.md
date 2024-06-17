422f98d25f3ee984ef0a67111b9a504c932ee37a
inoue0426 <sekihan_tennis@yahoo.co.jp>
2024-06-17 16:08:56 -0400

    update main function

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Script to get Drug Name CID, SID, SMILES, Target Name, and UniProt IDs from DrugBank
 
+[UPDATE]
+- 06/17/2024
+  - Add Target ID, PMIDs and PMID Count.
+
 ## How to use
 
 ```python
--- parser.py
+++ parser.py
@@ -1,99 +1,94 @@
-import argparse
 import xml.etree.ElementTree as ET
-
 import pandas as pd
 from tqdm import tqdm
+import argparse
 
-
-def parse_xml(xml_file_path):
-    print("Parsing XML file...")
-    tree = ET.parse(xml_file_path)
+def load_xml(xml_file):
+    tree = ET.parse(xml_file)
     root = tree.getroot()
-    namespace = "{http://www.drugbank.ca}"
-    drug_data_list = []
+    return root
 
-    for drug in tqdm(root.findall(f"{namespace}drug")):
-        drug_info = {
-            "Drug Name": (
-                drug.find(f"{namespace}name").text
-                if drug.find(f"{namespace}name") is not None
-                else "N/A"
-            ),
-            "DrugBank ID": None,
-            "PubChem CID": None,
-            "PubChem SID": None,
-            "SMILES": None,
-            "Targets Name": [],
-            "Targets": [],
-        }
-        _parse_drug_info(drug, namespace, drug_info)
-        drug_data_list.append(drug_info)
+def parse_drug(root, ns):
+    res = pd.DataFrame()
+    for drug in tqdm(root.findall('db:drug', ns)):
+        drug_name = drug.find('db:name', ns).text
+        drugbank_id = drug.find('db:drugbank-id', ns).text[2:]
+        res = parse_targets(drug, ns, res, drug_name, drugbank_id)
+    return res
 
-    return drug_data_list
-
-
-def _parse_drug_info(drug, namespace, drug_info):
-    print("Parsing drug information...")
-    _parse_drugbank_id(drug, namespace, drug_info)
-    _parse_external_identifiers(drug, namespace, drug_info)
-    _parse_calculated_properties(drug, namespace, drug_info)
-    _parse_targets(drug, namespace, drug_info)
-
-
-def _parse_drugbank_id(drug, namespace, drug_info):
-    print("Parsing DrugBank ID...")
-    drug_id_element = drug.find(f"{namespace}drugbank-id[@primary='true']")
-    if drug_id_element is not None:
-        drug_info["DrugBank ID"] = drug_id_element.text
-
-
-def _parse_external_identifiers(drug, namespace, drug_info):
-    print("Parsing external identifiers...")
-    external_identifiers = drug.find(f"{namespace}external-identifiers")
-    if external_identifiers is not None:
-        for identifier in external_identifiers:
-            resource = identifier.find(f"{namespace}resource")
-            id_value = identifier.find(f"{namespace}identifier")
-            if resource is not None and id_value is not None:
-                if resource.text == "PubChem Compound":
-                    drug_info["PubChem CID"] = id_value.text
-                elif resource.text == "PubChem Substance":
-                    drug_info["PubChem SID"] = id_value.text
-
-
-def _parse_calculated_properties(drug, namespace, drug_info):
-    calculated_properties = drug.find(f"{namespace}calculated-properties")
-    if calculated_properties is not None:
-        for prop in calculated_properties:
-            kind = prop.find(f"{namespace}kind")
-            value = prop.find(f"{namespace}value")
-            if kind is not None and value is not None and kind.text == "SMILES":
-                drug_info["SMILES"] = value.text
-
-
-def _parse_targets(drug, namespace, drug_info):
-    targets = drug.find(f"{namespace}targets")
+def parse_targets(drug, ns, res, drug_name, drugbank_id):
+    properties = {
+        'SMILES': get_property_value(drug, ns, 'SMILES'),
+        'PubChem Compound': get_property_value(drug, ns, 'PubChem Compound'),
+        'PubChem Substance': get_property_value(drug, ns, 'PubChem Substance')
+    }
+    targets = drug.find('db:targets', ns)
     if targets is not None:
-        for target in targets:
-            target_name = target.find(f"{namespace}name")
-            if target_name is not None:
-                drug_info["Targets Name"].append(target_name.text)
-            polypeptide = target.find(f"{namespace}polypeptide")
-            if polypeptide is not None and "id" in polypeptide.attrib:
-                drug_info["Targets"].append(polypeptide.attrib["id"])
+        for target in targets.findall('db:target', ns):
+            target_info = {
+                'ID': get_target_id(target, ns),
+                'Name': get_target_name(target, ns),
+                'UniProt ID': get_uniprot_id(target, ns),
+                'Gene Name': get_gene_name(target, ns)
+            }
+            res = parse_articles(target, ns, res, drug_name, drugbank_id, **properties, **target_info)
+    return res
 
+def get_property_value(element, ns, property_name):
+    return next((p.find('db:value', ns).text for p in element.findall(f'db:calculated-properties/db:property[db:kind="{property_name}"]', ns)), None) or ''
 
-def main():
-    parser = argparse.ArgumentParser(description="DrugBank XML Parser")
-    parser.add_argument("xml_file", help="Path to the DrugBank XML file")
-    parser.add_argument("output_file", help="Path to the output CSV file")
-    args = parser.parse_args()
+def get_target_id(target, ns):
+    return target.find('db:id', ns).text if target.find('db:id', ns) is not None else 'N/A'
 
-    drug_data_list = parse_xml(args.xml_file)
+def get_target_name(target, ns):
+    return target.find('db:name', ns).text if target.find('db:name', ns) is not None else 'N/A'
 
-    drug_df = pd.DataFrame(drug_data_list)
-    drug_df.to_csv(args.output_file, index=False)
+def get_uniprot_id(target, ns):
+    polypeptide = target.find('db:polypeptide', ns)
+    return polypeptide.get('id') if polypeptide is not None else 'N/A'
 
+def get_gene_name(target, ns):
+    polypeptide = target.find('db:polypeptide', ns)
+    if polypeptide is None:
+        return 'N/A'
+    gene_name = polypeptide.find('db:gene-name', ns)
+    return gene_name.text if gene_name is not None else 'N/A'
+
+def parse_articles(target, ns, res, drug_name, drugbank_id, **kwargs):
+    articles = target.findall('db:references/db:articles/db:article', ns)
+    pmids = [
+        article.find('db:pubmed-id', ns).text 
+        for article in articles 
+        if article.find('db:pubmed-id', ns) is not None and article.find('db:pubmed-id', ns).text is not None
+    ]
+    pubmed_ids = ";".join(pmids) if pmids else 'N/A'
+    res = pd.concat([res, pd.DataFrame({
+        'Drug': [drug_name], 
+        'DrugBank ID': [drugbank_id],
+        'SMILES': [kwargs['SMILES']],
+        'PubChem CID': [kwargs['PubChem Compound']],
+        'PubChem SID': [kwargs['PubChem Substance']],
+        'Target ID': [kwargs['ID']],
+        'Target Name': [kwargs['Name']],
+        'UniProt ID': [kwargs['UniProt ID']],
+        'Gene Name': [kwargs['Gene Name']],
+        'PMIDs': [pubmed_ids],
+        'PMID Count': [len(pmids)] 
+    })], ignore_index=True)
+    return res
+
+def save_to_csv(res, output_file):
+    res.to_csv(output_file, index=False)
 
 if __name__ == "__main__":
-    main()
+    print("Starting to parse DrugBank XML file...")
+    parser = argparse.ArgumentParser(description='Parse DrugBank XML file and save target information to CSV')
+    parser.add_argument('--input', default='drugbank.xml', help='Path to the DrugBank XML file to parse')
+    parser.add_argument('--output', default='targets.csv', help='Path to the output CSV file')
+    args = parser.parse_args()
+    root = load_xml(args.input)
+    ns = {'db': 'http://www.drugbank.ca'}
+    res = parse_drug(root, ns)
+    save_to_csv(res, args.output)
+    print("Parsing completed. Results saved to", args.output)
+
